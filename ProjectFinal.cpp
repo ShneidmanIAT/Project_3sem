845a8dfcf47d4ca85ec4acc200cf9bcfdbf09e5a
sHiNkO1975 <shinko.iv@gmail.com>
2022-12-19 00:37:54 +0300

    Changed data type to vector

--- ProjectFinal.cpp
+++ ProjectFinal.cpp
@@ -2,177 +2,199 @@
 //
 
 #include <iostream>
+#include <vector>
 
 template <typename T>
-class Matrix{
-	int m, n;
-	T** data;
+class Matrix final 
+{
+	int width = 1;
+	int length = 1;
+	std::vector< std::vector <T> > data;
+	
+	void SwapRow(int i, int j)
+	{
+		std::swap(this->data[i], this->data[j]);
+		return;
+	}
+		
 public:
-	Matrix() {
-		n = m = 0;
-		data = nullptr;
-		}
+	Matrix() 
+	{
+		T tmp = T(0);
+		std::vector<T> a;
+		a.push_back(tmp);
+		data.push_back(a);
+	}
 		
-	Matrix(int m, int n): n(n), m(m){
-		data = (T**) new T*[m];
-		for(int i=0;i<m;i++){
-			data[i] = (T*)new T[n];
+	Matrix(int width, int length): width(width), length(length){
+		std::vector<T> str;
+		for (int i=0;i<width;i++)
+		{	
+			for(int j=0;j<length; j++)
+			{	
+				str.push_back(T(0));
 			}
-		for (int i=0;i<m;i++){
-			for (int j=0;j<n;j++){
-				data[i][j] = 0;
-				}
-			}
+			data.push_back(str);
+			str.clear();
 		}
-	/*copy constructor*/
-	Matrix(const Matrix& other){
-		m = other.m;
-		n = other.n;
+	}
+
+	Matrix(size_t width, size_t length, std::vector< std::vector<T> > data):
+	length(length), width(width), data(data) {}
+	
+	Matrix(const Matrix& other)
+	{
+		width = other.width;
+		length = other.length;
 		
-		data = (T**) new T*[m];
-		for(int i=0;i<m;i++){
-			data[i] = (T*) new T[n];
-		}
-		for(int i=0;i<m;i++){
-			for(int j=0;j<n;j++){
-				data[i][j] = other.data[i][j];
-				}
+		for (int i=0;i<width;i++)
+		{
+			std::vector<T> data_str;
+			for (int j=0;j<length;j++)
+			{
+				data_str.push_back(other.data[i][j]);
 			}
+			data.push_back(data_str);
+			data_str.clear();
+		}
 	}
 	
-	T Getij(int i, int j){
-		if (i<0 or i>=m){
-			std::cerr<<"Index out of range\n";
+	T Getij(int i, int j)
+	{
+		try 
+		{
+			if(i<0 or i>=width or j<0 or j>=length)
+			{
+				std::cout<< "Index out of range. Can't get value." << std::endl;
+				throw 2;
 			}
-		if (i<0 or i>=n){
-			std::cerr<<"Index out of range\n";
-			}
-		return data[i][j];
+			return data[i][j];
 		}
-	
-	void Setij(int i, int j, T val){
-		if(i<0 or i>=m){
-			return;
-			}
-		if(j<0 or j>=n){
-			return;
-			}
-		data[i][j] = val;
+		catch(int a)
+		{
+			std::cerr<<"Wrong index. Error code 2\n";
+		}
 	}
-	// copy operator
+	
+	void Setij(int i, int j, T val)
+	{
+		try
+		{
+			if(i<0 or i>=width or j<0 or j>=length)
+			{
+				std::cout<< "Index out of range. Can't set value." << std::endl;
+				throw 2;
+			}
+			data[i][j] = val;	
+		}
+		catch(int a)
+		{
+			std::cerr<<"Wrong index. Error code 2\n";
+		}
+	}
+	
 	Matrix operator=(const Matrix& rhs){
-		if(n>0){
-			for (int i=0;i<m;i++){
-				delete[] data[i];
-				}
-			}
-		if(m>0){
-			delete[] data;
- 			}
- 			
- 		m = rhs.m;
- 		n = rhs.n;
- 		data = (T**) new T*[m];
-		for(int i=0;i<m;i++){
-			data[i] = (T*) new T[n];
-		}
-		for(int i=0;i<m;i++){
-			for(int j=0;j<n;j++){
-				data[i][j] = rhs.data[i][j];
-				}
-			}
+		if (&rhs == this)
+			return *this;
+		Matrix tmp(rhs);
+		std::swap(width, tmp.width);
+		std::swap(length, tmp.length);
+		std::swap(data, tmp.data);
 		return *this;
 	}
 	
-	Matrix operator+(const Matrix& other){
-		if (n != other.n or m != other.m){
-			std::cerr<<"Sizes of matrices in sum don't match\n";
-			return *this;
+	Matrix operator+(const Matrix& other) const
+	{
+		try
+		{
+			if(width != other.width or length != other.length)
+			{
+				std::cout<< "Matrices have different sizes. Can't sum." << std::endl;
+				throw 2;
 			}
-		Matrix sum(n, m);
-		for (int i=0;i<m;i++){
-			for (int j=0;j<n;j++){
-				sum.data[i][j] = data[i][j] + other.data[i][j];
+
+			std::vector<std::vector<T>> sum;
+			for (int i=0;i<width;i++)
+			{
+				std::vector<T> tmp;
+				for (int j=0;j<length;j++)
+				{
+					tmp.push_back(data[i][j]+other.data[i][j]);
 				}
+				sum.push_back(tmp);
 			}
-		return sum;
+			return Matrix(width, length, sum);
+		}
+		catch(int a)
+		{
+			std::cerr<<"Wrong size. Error code 2\n";
+		}
 	}
 	
 	Matrix operator*(T c){
-		Matrix prod(n, m);
-		for (int i=0;i<m;i++){
-			for (int j=0;j<n;j++){
+		Matrix prod(width, length);
+		for (int i=0;i<width;i++){
+			for (int j=0;j<length;j++){
 				prod.data[i][j] = c*data[i][j];
 				}
 			}
 		return prod;
 		}
 	
-	Matrix operator*(const Matrix& other){
-		if (n != other.m){
-			std::cerr<<"Sizes of matrices don't allow multiplication\n";
-			return *this;
-			} //
-		Matrix prod(m, other.n);
-		for (int i=0;i<m;i++){
-			for (int j=0;j<other.n;j++){
-				for (int k=0;k<n;k++){
-					prod.data[i][j]+=data[i][k]*other.data[k][j];
+	Matrix operator*(const Matrix& other) const
+	{
+		try
+		{
+			if (length != other.width)
+			{
+				throw 2;
+			}
+			Matrix prod(width, other.n);
+			for (int i=0;i<width;i++){
+				for (int j=0;j<other.length;j++){
+					for (int k=0;k<length;k++){
+						prod.data[i][j]+=data[i][k]*other.data[k][j];
+						}
 					}
 				}
-			}
-		return prod;
+			return prod;
 		}
+		catch(int a)
+		{
+			std::cerr<<"Wrong size. Errror code 2\n";
+		}
+	}
 		
-	void SwapRow(int i, int j){
-		std::swap(this->data[i], this->data[j]);
-		return;
-		}
-	
-	
-	Matrix Gauss(){
+	Matrix FWDGauss()
+	{
 		//pivot
-		for (int i=0;i<m;i++){
-			for (int k=i+1;k<m;k++){
-				if (data[i][i] == 0 and data[k][i] != 0){
+		for (int i=0;i<width;i++)
+		{
+			for (int k=i+1;k<width;k++)
+			{
+				if (data[i][i] == 0 and data[k][i] != 0)
 					this->SwapRow(i, k);
-					}
-				}
 			}
+		}
 		//forward_elim only
 		
-		for (int i=0;i<m-1;i++){
-			for (int j=i+1;j<m;j++){
+		for (int i=0;i<width-1;i++)
+		{
+			for (int j=i+1;j<width;j++){
 				if (this->data[i][i] == 0){return *this;}
 				T t = this->data[j][i]/this->data[i][i];
-				for (int k=0;k<n;k++){
+				for (int k=0;k<length;k++){
 					this->data[j][k] -= t*this->data[i][k];
 					}
 				}
-			}
-			
+		}	
 		return *this;
-		}
-	
-	
-	Matrix* SolSpace(); //TODO
-	
-	T* EigenVal(); //TODO
-	
-	Matrix EigenVec(T val){
-		Matrix tmp(*this); // m = n
-		for (int i=0;i<m;i++){
-			tmp.data[i][i]-=val;
-			}
-		tmp.Gauss();
-		//... <- SolSpace
-		return tmp;
 	}
 	
 	void Print(){
-		for(int i=0;i<m;i++){
+		for(int i=0;i<width;i++){
 			std::cout<<"|";
-			for(int j=0;j<n;j++){
+			for(int j=0;j<length;j++){
 				std::cout<<data[i][j]<<" ";
 				}
 			std::cout<<"|\n";
@@ -180,16 +202,7 @@ public:
 		std::cout<<"\n";
 	}
 		
-	~Matrix(){
-		if(n>0){
-			for (int i=0;i<m;i++){
-				delete[] data[i];
-				}
-			}
-		if(m>0){
-			delete[] data;
- 			}
-	}
+	~Matrix(){}
 };
 
 int main()
@@ -197,12 +210,11 @@ int main()
 	Matrix<float> h(3, 4);
 	for (int i=0;i<=3;i++){
 		for (int j=0;j<3;j++){
-			h.Setij(i, j, (i)*(j));
+			h.Setij(i, j, (i)*(j)+1);
 			}
 		}
 	h.Print();
-	h = h.EigenVec(1);
-	h.Print();
+	h.FWDGauss().Print();
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
