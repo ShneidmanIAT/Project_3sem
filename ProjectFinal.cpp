d4519287e02e24fe5cf4e7eb0d83223a0cee70fb
sHiNkO1975 <shinko.iv@gmail.com>
2022-12-20 01:17:33 +0300

    Matrix creates using template with given size

--- ProjectFinal.cpp
+++ ProjectFinal.cpp
@@ -4,11 +4,9 @@
 #include <iostream>
 #include <vector>
 
-template <typename T>
+template <typename T, int width, int length>
 class Matrix final 
 {
-	int width = 1;
-	int length = 1;
 	std::vector< std::vector <T> > data;
 	
 	void SwapRow(int i, int j)
@@ -18,15 +16,8 @@ class Matrix final
 	}
 		
 public:
-	Matrix() 
-	{
-		T tmp = T(0);
-		std::vector<T> a;
-		a.push_back(tmp);
-		data.push_back(a);
-	}
 		
-	Matrix(int width, int length): width(width), length(length){
+	Matrix(){
 		std::vector<T> str;
 		for (int i=0;i<width;i++)
 		{	
@@ -39,14 +30,10 @@ public:
 		}
 	}
 
-	Matrix(size_t width, size_t length, std::vector< std::vector<T> > data):
-	length(length), width(width), data(data) {}
+	Matrix(std::vector< std::vector<T> > data): data(data) {}
 	
 	Matrix(const Matrix& other)
-	{
-		width = other.width;
-		length = other.length;
-		
+	{		
 		for (int i=0;i<width;i++)
 		{
 			std::vector<T> data_str;
@@ -59,21 +46,9 @@ public:
 		}
 	}
 	
-	T Getij(int i, int j)
+	T Getij(int i, int j) const
 	{
-		try 
-		{
-			if(i<0 or i>=width or j<0 or j>=length)
-			{
-				std::cout<< "Index out of range. Can't get value." << std::endl;
-				throw 2;
-			}
-			return data[i][j];
-		}
-		catch(int a)
-		{
-			std::cerr<<"Wrong index. Error code 2\n";
-		}
+		return data[i][j];
 	}
 	
 	void Setij(int i, int j, T val)
@@ -93,76 +68,54 @@ public:
 		}
 	}
 	
-	Matrix operator=(const Matrix& rhs){
+	template<int wid, int len>	
+	Matrix<T, wid, len> operator=(const Matrix<T, wid, len>& rhs){
 		if (&rhs == this)
 			return *this;
 		Matrix tmp(rhs);
-		std::swap(width, tmp.width);
-		std::swap(length, tmp.length);
 		std::swap(data, tmp.data);
 		return *this;
 	}
 	
-	Matrix operator+(const Matrix& other) const
+	Matrix operator+(const Matrix<T, width, length>& other) const
 	{
-		try
+		std::vector<std::vector<T>> sum;
+		for (int i=0;i<width;i++)
 		{
-			if(width != other.width or length != other.length)
+			std::vector<T> tmp;
+			for (int j=0;j<length;j++)
 			{
-				std::cout<< "Matrices have different sizes. Can't sum." << std::endl;
-				throw 2;
+				tmp.push_back(data[i][j]+other.data[i][j]);
 			}
-
-			std::vector<std::vector<T>> sum;
-			for (int i=0;i<width;i++)
-			{
-				std::vector<T> tmp;
-				for (int j=0;j<length;j++)
-				{
-					tmp.push_back(data[i][j]+other.data[i][j]);
-				}
-				sum.push_back(tmp);
-			}
-			return Matrix(width, length, sum);
+			sum.push_back(tmp);
 		}
-		catch(int a)
-		{
-			std::cerr<<"Wrong size. Error code 2\n";
-		}
+		return Matrix(sum);
 	}
 	
-	Matrix operator*(T c){
-		Matrix prod(width, length);
+	Matrix operator*(T c)
+	{
+		Matrix prod;
 		for (int i=0;i<width;i++){
 			for (int j=0;j<length;j++){
 				prod.data[i][j] = c*data[i][j];
 				}
 			}
 		return prod;
-		}
+	}
 	
-	Matrix operator*(const Matrix& other) const
+	template<int wid, int len>
+	Matrix<T, width, len> operator*(const Matrix<T, wid, len>& other) const
 	{
-		try
-		{
-			if (length != other.width)
-			{
-				throw 2;
-			}
-			Matrix prod(width, other.n);
-			for (int i=0;i<width;i++){
-				for (int j=0;j<other.length;j++){
-					for (int k=0;k<length;k++){
-						prod.data[i][j]+=data[i][k]*other.data[k][j];
-						}
+		static_assert(length == wid, "Matrix dimensions mismatched");
+		Matrix<T, width, len> prod;
+		for (int i=0;i<width;i++){
+			for (int j=0;j<len;j++){
+				for (int k=0;k<length;k++){
+					prod.Setij(i, j, prod.Getij(i,j)+data[i][k]*other.Getij(k,j));
 					}
 				}
-			return prod;
-		}
-		catch(int a)
-		{
-			std::cerr<<"Wrong size. Errror code 2\n";
-		}
+			}
+		return prod;
 	}
 		
 	Matrix FWDGauss()
@@ -191,14 +144,16 @@ public:
 		return *this;
 	}
 	
-	void Print(){
-		for(int i=0;i<width;i++){
+	void Print()
+	{
+		for(int i=0;i<width;i++)
+		{
 			std::cout<<"|";
 			for(int j=0;j<length;j++){
 				std::cout<<data[i][j]<<" ";
 				}
 			std::cout<<"|\n";
-			}
+		}
 		std::cout<<"\n";
 	}
 		
@@ -207,8 +162,8 @@ public:
 
 int main()
 {
-	Matrix<float> h(3, 4);
-	for (int i=0;i<=3;i++){
+	Matrix<float, 3, 4> h;
+	for (int i=0;i<3;i++){
 		for (int j=0;j<3;j++){
 			h.Setij(i, j, (i)*(j)+1);
 			}
